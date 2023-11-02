50fe487693454900a74a6d2671b8588e778b9495
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-11-02 19:38:48 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 
     nargs = len(sys.argv) - 1
-    if lnargs != 3:
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
