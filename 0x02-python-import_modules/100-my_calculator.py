6627ca8a00b836a85a7b28c338a7a9babcb7fcbb
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-11-02 19:38:19 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
-    if len(sys.argv) != 4:
+
+    nargs = len(sys.argv) - 1
+    if lnargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -16,10 +17,10 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     
     if op == '+':
-        print("{} + {} = {}".format(a, b, add(a + b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == '-':
-        print("{} - {} = {}".format(a, b, sub(a - b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == '*':
-        print("{} * {} = {}".format(a, b, mul(a * b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
-        print("{} / {} = {}".format(a, b, div(a / b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
