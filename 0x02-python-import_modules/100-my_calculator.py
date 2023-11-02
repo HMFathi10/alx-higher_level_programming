4e769f509a2196d2e1f97b15d81e0159c3c68e5b
Hossam Mahmoud <hossamfci10@gmail.com>
2023-11-02 09:30:44 -0700

    Added

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
         from calculator_1 import add, sub, mul, div
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        match sys.argv[2]:
-            case '+':
-                print("{} + {} = {}".format(a, b, a + b))
-            case '-':
-                print("{} - {} = {}".format(a, b, a - b))
-            case '*':
-                print("{} * {} = {}".format(a, b, a * b))
-            case '/':
-                print("{} / {} = {}".format(a, b, a / b))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+        op = sys.argv[2]
+        if op == '+':
+            print("{} + {} = {}".format(a, b, a + b))
+        elif op == '-':
+            print("{} - {} = {}".format(a, b, a - b))
+        elif op == '*':
+            print("{} * {} = {}".format(a, b, a * b))
+        elif op == '/':
+            print("{} / {} = {}".format(a, b, a / b))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
