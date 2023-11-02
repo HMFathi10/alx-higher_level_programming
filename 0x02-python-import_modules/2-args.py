2d88ace2fae0e306d265865021ae0363b962e891
HMFathi10 <hossamfci10@gmail.com>
2023-11-02 15:14:02 +0200

    Added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("1 :{}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv)))
-        for i in range(1, len(sys.argv)):
+        for i in range(1, len(sys.argv) + 1):
             print("{}: {}".format(i, sys.argv[i]))
