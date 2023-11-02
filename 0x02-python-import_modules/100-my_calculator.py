ccf0271987174f17578aa9f8f4da550e2679915b
Hossam Mahmoud <hossamfci10@gmail.com>
2023-11-02 10:40:00 -0700

    Added

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
+
     if op == '+':
         print("{} + {} = {}".format(a, b, a + b))
     elif op == '-':
