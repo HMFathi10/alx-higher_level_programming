e3131dfd6a3f9d4246adc312e8bbaf43d5508abc
HMFathi10 <hossamfci10@gmail.com>
2023-11-05 16:14:14 +0200

    Added

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    x1 = x2 = 0
+    y1 = y2 = 0
+    for i in range(len(tuple_a)):
+        if i == 0:
+            x1 = tuple_a[i]
+        else:
+            y1 = tuple_a[i]
+    for j in range(len(tuple_b)):
+        if j == 0:
+            x2 = tuple_b[j]
+        else:
+            y2 = tuple_b[j]
+    new_tuple = (x1 + x2, y1 + y2)
+    return (new_tuple)
