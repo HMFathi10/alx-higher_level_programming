8b114e1462ec4f14b6d31f1796f4809edb4a558a
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-11-05 13:57:37 +0200

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = my_string[:]
+    count = 0
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            new_string = new_string[:i] + my_string[i + 1:]
+            new_string = new_string[:i - count] + my_string[i + 1:]
+            count += 1
     return (new_string)
