c3a51c467084592004bfb7ed7bf19cfc03fbf3fb
Hossam Mahmoud <hossamfci10@gmail.com>
2023-11-20 03:44:03 -0800

    Added

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             result += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return (result)
+    print()
+    return (result)
