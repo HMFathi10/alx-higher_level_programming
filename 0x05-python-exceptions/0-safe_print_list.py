c67300f1bc67001afac266536f223092c4feac1b
Hossam Mahmoud <hossamfci10@gmail.com>
2023-11-20 02:53:25 -0800

    Added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(x):
             print("{}".format(my_list[idx]), end="")
-        idx++
+        idx += 1
     except:
         pass
     finally:
