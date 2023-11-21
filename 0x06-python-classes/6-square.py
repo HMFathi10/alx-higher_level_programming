ce4ac83045d38407603195bba123d9ace6272cab
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-11-21 11:22:31 +0200

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,12 +73,12 @@ class Square:
     def my_print(self):
         """ Print the square"""
 
-        if !self.__size:
+        if not self.__size:
             print()
             return
 
         [print("") for i in range(0, self.__position[1])]
         for x in range(0, self.__size):
-            [print(" ") for sp in range(0, self.__position[0])]
-            [print("#") for ch in range(0, self.__size)]
+            [print(" ", end="") for sp in range(0, self.__position[0])]
+            [print("#", end="") for ch in range(0, self.__size)]
             print()
