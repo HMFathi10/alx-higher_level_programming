1f3d624a0b8a0cb9008c783d36f2e36614aa5575
HMFathi10 <hossamfci10@gmail.com>
2023-11-28 08:53:11 +0200

    Added

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,8 +18,14 @@ def text_indentation(text):
 
     if type(text) is not str:
         raise TypeError("text must be a string")
-    new_text = text[:]
-    new_text = new_text.replace(".", "\n\n")
-    new_text = new_text.replace("?", "\n\n")
-    new_text = new_text.replace(":", "\n\n")
-    print(new_text, end="")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
