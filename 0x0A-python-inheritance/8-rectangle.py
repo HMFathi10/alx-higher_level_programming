a7f4ebdf833ad2acef23e7609f8a053299b74e71
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-12-04 14:28:11 +0200

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     def __inti__(self, width, height):
         """Rectangle that inherits from BaseGeometry"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        BaseGeometry.integer_validator("width", width)
+        BaseGeometry.integer_validator("height", height)
         self.width = width
         self.height = height
