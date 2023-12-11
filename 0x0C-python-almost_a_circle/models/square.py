161f05fde8a52c28fdecd8761125d18c368ccba0
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2023-12-11 19:41:14 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,43 +39,45 @@ class Square(Rectangle):
             **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
-            for i in range(len(args)):
-                match i:
-                    case 0:
-                        if args[i] is None:
-                            self.__init__(self.size,
-                                          self.x, self, y)
-                        else:
-                            self.id = args[i]
-                    case 1:
-                        self.size = args[i]
-                    case 2:
-                        self.x = args[i]
-                    case 3:
-                        self.y = args[i]
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
 
         elif kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "id" and value is None:
-                    self.__init__(self.size, self.x, self.y)
-                elif key == "id":
-                    self.id = value
-                elif key == "size":
-                    self.size = value
-                elif key == "x":
-                    self.x = value
-                elif key == "y":
-                    self.y = value
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {
-                "id": self.id,
-                "size": self.width,
-                "x": self.x,
-                "y": self.y
-                }
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
 
     def __str__(self):
+        """Return the print() and str() representation of a Square."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
