4b3189051b68d102c061b63b70b72d98fdf86d1f
HMFathi10 <hossamfci10@.com>
2024-02-12 09:12:08 +0200

    Added

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
