fe1cd3697093c24d29c8be59d9ab5d06aa992922
Hossam Mahmoud <52832130+HMFathi10@users.noreply.github.com>
2024-02-14 09:01:22 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-module.exports = class Square extends require ('./4-rectangle.js') {
-	constructor (size) {
-		super(size, size);
-	}
-
-	charPrint (c = 'X') {
-		for (let i in range(this.size)
-			console.log(c.repeat(this.size));
-	}
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
 };
